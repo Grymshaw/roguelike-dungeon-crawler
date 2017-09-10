4726022574ecd94d3eb810f0bb411955e2de4ac6
Kyle Grymonpre <kgrymonpr@gmail.com>
2017-09-10 14:58:50 -0500

    Add functionality for maze generator

--- src/lib/maze-generator.js
+++ src/lib/maze-generator.js
@@ -1,15 +1,15 @@
 export const getPotentialMoves = (width, height, coords) => {
   const x = coords[0],
-    y = coords[1];
+        y = coords[1];
 
   if(x < 0 || x >= width || y < 0 || y >= height) {
-    return;
+    throw(new Error('Error: invalid arguments'));
   }
 
   const possibleMoves = [ [-1, 0], [0, -1], [1, 0], [0, 1] ];
   return possibleMoves.reduce((acc, move) => {
-    if (x + move[0] >= 0 && x + move[0] < width && y + move[1] >= 0 && y + move[1] < height)
-      acc.push( [ x + move[0], y + move[1] ] )
+    (x + move[0] >= 0 && x + move[0] < width && y + move[1] >= 0 && y + move[1] < height)
+      && acc.push([x + move[0], y + move[1]]);
     return acc;
   }, []);
 };
@@ -19,7 +19,7 @@ export const getEmptySquare = (map) => {
   for(let y = 1; y < map.length; y += 2) {
     for(let x = 1; x < map[y].length; x += 2) {
       const potentialMoves = getPotentialMoves(map[y].length, map.length, [x, y]);
-      if(potentialMoves.filter( move => map[move[1]][move[0]] ).length === 0) {
+      if(potentialMoves.filter(move => map[move[1]][move[0]] ).length === 0) {
         return [x, y];
       }
     }
@@ -28,64 +28,83 @@ export const getEmptySquare = (map) => {
 };
 
 
-// export const getRandomRoom = (map, maxWidth, maxHeight) => {
-//   const MAX_ATTEMPTS = 200;
-//   const width = ((Math.random() * maxWidth / 2) | 0) * 2 + 1;
-//   const height = ((Math.random() * maxHeight / 2) | 0) * 2 + 1;
-
-//   let found = false;
-//   let origin;
-//   for(let i = 0; i < MAX_ATTEMPTS && !found; i++) {
-//     origin = getEmptySquare(map);
-//     if(origin[0] + height < map[0].length && origin[1] + height < map.length) {
-//       found = true;
-//     }
-//   }
-
-//   if(!found) return { origin: [-1, -1], width: 0, height: 0 };
-
-//   return { origin, width, height };
-// };
-
-
-export const placeRoom = (map, width, height) => {
-  const MAX_ATTEMPTS = 200;
-
-  let isPlaced = false;
-  let isBlocked = false;
-
-  let origin;
-
-  for(let i = 0; i < MAX_ATTEMPTS && !isPlaced; i++) {
-    origin = getRandomOrigin(map[0].length, map.length, width, height);
-    for(let y = origin[1]; y < origin[1] + height && !isBlocked; y++) {
-      for (let x = origin[0]; x < origin[0] + width && !isBlocked; x++) {
-        if(map[y][x]) isBlocked = true;
-      }
+export const initializeEmptyMap = (width, height) => {
+  const map = [];
+  for(let y = 0; y < height; y++) {
+    const row = [];
+    for(let x = 0; x < width; x++) {
+      row.push(false);
     }
-    if(!isBlocked) isPlaced = true;
+    map.push(row);
   }
+  return map;
+};
 
-  // place the room in the map
-  if(isPlaced) {
-    const newMap = map.map(row => row.map(val => val));
-    for(let y = origin[1]; y < origin[1] + height; y++) {
-      for (let x = origin[0]; x < origin[0] + width; x++) {
+
+export const placeRoom = (map, origin, roomWidth, roomHeight) => {
+  if(origin[0] % 2 === 0 || origin[1] % 2 === 0 || roomWidth % 2 === 0 || roomHeight % 2 === 0)
+    throw(new Error('Invalid arguments -- coords and dimensions must be odd'));
+
+  let isPlaceable = true;
+  for(let y = origin[1]; y < roomHeight && isPlaceable; y++) {
+    for(let x = origin[0]; x < roomWidth && isPlaceable; x++) {
+      isPlaceable = isPlaceable && !map[y][x];
+    }
+  }
+  if (isPlaceable) {
+    const newMap = Object.assign([], map);
+    for(let y = origin[1]; y < roomHeight && isPlaceable; y++) {
+      for(let x = origin[0]; x < roomWidth && isPlaceable; x++) {
         newMap[y][x] = true;
       }
     }
     return newMap;
-  } else {
-    return map;
   }
 
-  // indicate the generation is finished
-  //TODO: Make it so that dungeon generation is finished (ie. in Redux store)
+  return false;
+
 };
 
+// export const placeRoom = (map, width, height) => {
+//   const MAX_ATTEMPTS = 200;
 
-export const getRandomOddNumber = (max) => {
-  return ((Math.random() * max / 2) | 0) * 2 + 1;
+//   let isPlaced = false;
+//   let isBlocked = false;
+
+//   let origin;
+
+//   for(let i = 0; i < MAX_ATTEMPTS && !isPlaced; i++) {
+//     origin = getRandomOrigin(map[0].length, map.length, width, height);
+//     for(let y = origin[1]; y < origin[1] + height && !isBlocked; y++) {
+//       for (let x = origin[0]; x < origin[0] + width && !isBlocked; x++) {
+//         if(map[y][x]) isBlocked = true;
+//       }
+//     }
+//     if(!isBlocked) isPlaced = true;
+//   }
+
+//   // place the room in the map
+//   if(isPlaced) {
+//     const newMap = map.map(row => row.map(val => val));
+//     for(let y = origin[1]; y < origin[1] + height; y++) {
+//       for (let x = origin[0]; x < origin[0] + width; x++) {
+//         newMap[y][x] = true;
+//       }
+//     }
+//     return newMap;
+//   } else {
+//     return map;
+//   }
+
+//   // TODO: condsider returning object instead of arary { didFail: bool, map: arr }
+
+//   // indicate the generation is finished
+//   //TODO: Make it so that dungeon generation is finished (ie. in Redux store)
+// };
+
+
+export const getRandomOddNumber = (max, random = Math.random()) => {
+  return ((random * max / 2) | 0) * 2 + 1;
 };
 
 export const getRandomOrigin = (maxWidth, maxHeight, roomWidth, roomHeight) => {
